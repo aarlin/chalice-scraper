0f5c54628c8795753abc869e0524d0a9a3e04cf5
Aaron Lin <aaronlin256@gmail.com>
2021-04-25 10:15:08 -0400

    add discord endpoint wip

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 from chalice import Chalice, Rate
-from requests_html import HTMLSession
-import json
+import requests
 
-app = Chalice(app_name='scrape')
+app = Chalice(app_name='twilio_to_discord')
 
+@app.route('/sms', methods=['POST'])
+def periodic_task():
+    print(app.current_request.json_body)
+    body = request.values.get('Body', None)
+    number = request.form['From']
+    message_body = request.form['Body']
+
+    url = "<DISCORD_WEBHOOK_URL>"
+
+    data["content"] = message_body
+    data["username"] = number
+
+    result = requests.post(url, json = data)
+
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print("Payload delivered successfully, code {}.".format(result.status_code))
+
+    return "200"
+    
 # Automatically runs every 5 minutes
 # @app.schedule(Rate(5, unit=Rate.MINUTES))
 @app.route("/local-test")
